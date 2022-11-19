bfbcc901258a2d71c45cb3430346b35c904746a9
Симутин Андрей Сергеевич <assimutin@mts.ru>
2022-11-19 21:23:53 +0300

    +

---
+++ src/components/ScoreTable.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react';
+
+export default function ScoreTable() {
+
+const players = ['a', 'b', 'c'];
+const [removedRows, setRemovedRows] = useState(new Set());
+const [rows, setRows] = useState([['1','0','1'], ['0','1','0']]);
+
+const addRow = () => {
+    let arr =  players.map(p => '');
+    setRows([...rows, arr]);
+}
+
+const removeRow = (index) => {
+   if(removedRows.has(index))
+        removedRows.delete(index)
+    else
+        removedRows.add(index);
+    
+    setRemovedRows(new Set(removedRows));
+}
+
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    {players.map(player => (
+                        <td key={player}>{player}</td>
+                    ))}
+                </tr>
+            </thead>
+        
+            <tbody>
+            {rows.map((row, rIndex) => (
+                <tr key={'rows' + rIndex}>
+                    {row.map((col, cIndex) => (
+                            <td key={players[cIndex] + rIndex}>
+                                {removedRows.has(rIndex) ? '-' : col}
+                            </td>
+                        ))
+                    }
+                    <td>
+                        <button onClick={() => removeRow(rIndex)}>-</button>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        <button onClick={addRow}>+</button>
+    </div>
+  )
+}
