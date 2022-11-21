e146752fec77f2a37fa9a0698792badbbf5bb3d6
Симутин Андрей Сергеевич <assimutin@mts.ru>
2022-11-22 01:47:39 +0300

    adaptive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,12 +9,10 @@ export default function Sidebar() {
 
   const setActive = ({isActive}) => isActive ? 'nav-link active' : 'nav-link text-white'
 
-  // "nav-link active"
-  // "nav-link text-white"
   return (
-    <Container fluid className=''>
-      <Row className=''>
-        <Col sm='3' xs='12' className='px-0'>
+    <Container fluid>
+      <Row>
+        <Col sm='3' xs='12' xl='2' className='px-0'>
           <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark vh-sm-100">
             <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
               <span className="fs-4">1000</span>
