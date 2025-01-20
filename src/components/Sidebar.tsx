4ee59e008ab49e43eb6bdf0dcc293a3730a9345f
Simutin Andrey <simutin.andrey2@wb.ru>
2025-01-20 23:14:37 +0300

    game table title

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,7 +18,7 @@ import Col from 'react-bootstrap/Col'
         <Col sm='3' xs='12' xl='2' className='px-0'>
           <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark vh-sm-100">
             <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-              <span className="fs-4">1000</span>
+              <span className="fs-4">Игровая таблица</span>
             </a>
             <hr />
             <ul className="nav nav-pills flex-column mb-auto">
