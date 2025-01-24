30c2b365143ab13b0e4136c4db4ce3b5a59dfd6a
Simutin Andrey <simutin.andrey2@wb.ru>
2025-01-24 12:03:55 +0300

    GameHistoryPage

--- src/components/GameHistoryPage.tsx
+++ src/components/GameHistoryPage.tsx
@@ -1,42 +1,51 @@
-import React from 'react'
+import React from 'react';
 import ls from '../store/localStorageWrapper';
-import { Navigate, useParams, useNavigate } from 'react-router-dom';
 import GameTable from './UI/GameTable';
 import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 
 interface GameHistoryPageProps {
   gameName: string;
 }
 
 const GameHistoryPage: React.FC<GameHistoryPageProps> = ({ gameName }) => {
+  const router = useRouter();
 
-  const navigate = useNavigate();
+  // Получаем сохраненные игры
+  const saves = ls.getSavedGames();
 
-  const saves = ls.getSavedGames()
-  
-  const gameData = saves.get(gameName)
-  if (!gameData)
-    return (
-      <Navigate to={'/history'}></Navigate>
-    )
+  // Проверяем, существует ли игра
+  const gameData = saves.get(gameName);
 
-  const { players, rows, removedRows, savedRows } = gameData
-
-  const routeToHistory = () => {
-    navigate('/history')
+  // Если данных для игры нет, перенаправляем на страницу истории
+  if (!gameData) {
+    if (typeof window !== 'undefined') {
+      router.replace('/history');
+    }
+    return null; // Возвращаем null во время редиректа
   }
 
+  // Данные игры
+  const { players, rows, removedRows, savedRows } = gameData;
+
+  // Функция для возврата к истории
+  const routeToHistory = () => {
+    router.push('/history');
+  };
+
   return (
-    <>
-      <Button className='mb-3' onClick={routeToHistory}>{'<---'}</Button>
-      <GameTable
-        players={players}
-        rows={rows}
-        removedRows={new Set(removedRows)}
-        savedRows={new Set(savedRows)}
-      />
-    </>
-  )
-}
+      <>
+        <Button className="mb-3" onClick={routeToHistory}>
+          {'<---'}
+        </Button>
+        <GameTable
+            players={players}
+            rows={rows}
+            removedRows={new Set(removedRows)}
+            savedRows={new Set(savedRows)}
+        />
+      </>
+  );
+};
 
 export default GameHistoryPage;
