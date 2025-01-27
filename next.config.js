5a011bd281bd2ce63026546aa514e9abf6b619d0
Simutin Andrey <simutin.andrey2@wb.ru>
2025-01-27 23:35:10 +0300

    Revert "js obfusc"
    This reverts commit 97fd3f4381e24b024be42bdd9b647afca91b15bd.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const JavaScriptObfuscator = require('webpack-obfuscator');
-
 const nextConfig = {
     async redirects() {
         return [
@@ -10,24 +8,6 @@ const nextConfig = {
                 permanent: true, // Если это постоянное перенаправление (301), установите true
             },
         ];
-    },
-
-    webpack(config, { isServer, dev }) {
-        if (!isServer && !dev) {
-            // Обфускация только для client-side и только в production
-            config.plugins.push(
-                new JavaScriptObfuscator(
-                    {
-                        rotateStringArray: true,
-                        stringArray: true,
-                        stringArrayEncoding: ['base64'],
-                        stringArrayThreshold: 0.75,
-                    },
-                    []
-                )
-            );
-        }
-        return config;
     },
 };
 
