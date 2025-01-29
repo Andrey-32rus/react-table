fd119190a2365f326abd17ade5a4b6a3ac11be21
Simutin Andrey <simutin.andrey2@wb.ru>
2025-01-29 19:55:50 +0300

    no reopen

--- public/sidebar-toggle.js
+++ public/sidebar-toggle.js
@@ -1,10 +1,6 @@
 // Toggle the side navigation
 const sidebarToggle = document.body.querySelector('#sidebarToggle');
 if (sidebarToggle) {
-    // Uncomment Below to persist sidebar toggle between refreshes
-    if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        document.body.classList.toggle('sb-sidenav-toggled');
-    }
     sidebarToggle.addEventListener('click', event => {
         event.preventDefault();
         document.body.classList.toggle('sb-sidenav-toggled');
