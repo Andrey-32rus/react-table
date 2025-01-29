9cb19bfdbfedd4097fe9dd0d9e2ad7954b979030
Simutin Andrey <simutin.andrey2@wb.ru>
2025-01-29 19:15:32 +0300

    react table

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -39,9 +39,8 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         {/*<!-- Navbar Brand-->*/}
         <a className="navbar-brand ps-3" href="/">React Table</a>
         {/*<!-- Sidebar Toggle-->*/}
-        {/*   href="#!"  */}
         <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle">
-          <FontAwesomeIcon icon={faBars} />
+          <FontAwesomeIcon icon={faBars}/>
         </button>
         {/*<!-- Navbar Search-->*/}
         <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
@@ -49,15 +48,19 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         {/*<!-- Navbar-->*/}
         <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
-               aria-expanded="false"><FontAwesomeIcon icon={faUser} fixedWidth /></a>
+            <a
+              className="nav-link dropdown-toggle d-flex align-items-center"
+              id="navbarDropdown"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <span className="me-2">{session?.user?.name}</span>
+              <FontAwesomeIcon icon={faUser} fixedWidth/>
+            </a>
             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-              <li><a className="dropdown-item" href="#!">Settings</a></li>
-              <li><a className="dropdown-item" href="#!">Activity Log</a></li>
-              <li>
-                <hr className="dropdown-divider"/>
-              </li>
-              <li><a className="dropdown-item" href="#!">Logout</a></li>
+              <li><a className="dropdown-item" href="/">Logout</a></li>
             </ul>
           </li>
         </ul>
