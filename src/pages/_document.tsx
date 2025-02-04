3487e736fd9e2cbab3331f3ad1b47c3f62678753
Simutin Andrey <simutin.andrey2@wb.ru>
2025-02-04 19:32:57 +0300

    y metrica

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -23,6 +24,24 @@ class MyDocument extends Document {
           />
           <meta name="keywords" content="table, game, react, pwa"/>
           <meta name="author" content="AS"/>
+          <Script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(99795010, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });`,
+            }}
+          />
         </Head>
         <body>
         <Main/>
